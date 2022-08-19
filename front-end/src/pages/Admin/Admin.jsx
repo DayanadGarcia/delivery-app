dd86f4f0d430298d33f1d1047d2bfd3fdd539309
Dayana <dayanadiasgarcia@gmail.com>
2022-08-19 16:37:10 -0300

    add style to adm page

--- front-end/src/pages/Admin/Admin.jsx
+++ front-end/src/pages/Admin/Admin.jsx
@@ -62,50 +62,69 @@ export default function Admin() {
       <Navbar />
       <form>
 
-        <label htmlFor="name">
+        <label
+          htmlFor="name"
+          className="ml-2"
+        >
           Nome
           <input
             type="text"
             id="name"
-            placeholder="Nome e sobrenome"
+            className="bg-gray-200 p-2 rounded-sm mt-6 ml-2
+            mb-6 font-semibold outline-none"
             data-testid="admin_manage__input-name"
             value={ name }
             onChange={ ({ target }) => setName(target.value) }
           />
         </label>
 
-        <label htmlFor="email">
+        <label
+          htmlFor="email"
+          className="ml-2"
+        >
           Email
           <input
             type="text"
             id="email"
-            placeholder="seu-email@site.com.br"
+            className="bg-gray-200 p-2 rounded-sm mt-6 ml-2
+            mb-6 font-semibold outline-none"
             data-testid="admin_manage__input-email"
             value={ email }
             onChange={ ({ target }) => setEmail(target.value) }
           />
         </label>
 
-        <label htmlFor="password">
+        <label
+          htmlFor="password"
+          className="ml-2"
+        >
           Senha
           <input
             type="password"
             id="password"
-            placeholder="************"
+            className="bg-gray-200 p-2 rounded-sm mt-6 ml-2 font-semibold outline-none"
             data-testid="admin_manage__input-password"
             value={ password }
             onChange={ ({ target }) => setPassword(target.value) }
           />
         </label>
 
-        <label htmlFor="role">
+        <label
+          htmlFor="role"
+          className="ml-2"
+        >
           Tipo
           <select
+            className="ml-2 py-2.5 px-0 text-sm mt-6 text-[18px]
+          text-stone bg-transparent border-b-2
+          border-gray-200 appearance-none dark:text-gray-400
+          dark:border-gray-700 focus:outline-none focus:ring-0
+          focus:border-gray-200 peer"
             data-testid="admin_manage__select-role"
             value={ role }
             onChange={ handleChange }
           >
-            <option value="seller">seller</option>
+            <option selected value="seller">seller</option>
             <option value="customer">customer</option>
             <option value="administrator">administrator</option>
 
@@ -115,6 +134,17 @@ export default function Admin() {
         <button
           type="submit"
           data-testid="admin_manage__button-register"
+          className="
+          p-2 rounded-lg
+          font-semibold
+          outline-none
+          disabled:bg-gray-300
+          disabled:text-gray-500
+          font-bold
+          bg-[#07A0C3]
+          text-white
+          flex
+          items-center mx-auto"
           disabled={ isButtonDisabled }
           onClick={ handleRegister }
         >
